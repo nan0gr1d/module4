bee9a16aa73e8d7ad7c48b28c120d130920a8ad3
Chernov Andrei <chernovitus@gmail.com>
2024-07-09 19:37:39 +0300

    Main module - calling

---
+++ fake_math.py
@@ -0,0 +1,7 @@
+#fake_math
+
+def divide(n1, n2):
+    if n2 == 0:
+        return 'Ошибка'
+    else:
+        return n1 / n2
---
+++ true_math.py
@@ -0,0 +1,7 @@
+#true_math
+
+def divide(n1, n2):
+    if n2 == 0:
+        return 'infinity'
+    else:
+        return n1 / n2
